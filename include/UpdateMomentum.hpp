aa135e3dd1aa694398f9661fbefbff8f9b8cf79e
Dominik <rolfes1dominik@gmail.com>
2025-06-11 17:05:10 +0200

    beginning of the Gauge Force Updating

--- include/UpdateMomentum.hpp
+++ include/UpdateMomentum.hpp
@@ -23,18 +23,41 @@ public:
   using GaugeFieldType = typename DGaugeFieldType::type;
   using AdjFieldType = typename DAdjFieldType::type;
   static constexpr size_t rank =
-      DeviceGaugeFieldTypeTraits<GaugeFieldType>::rank;
+      DeviceGaugeFieldTypeTraits<GaugeFieldType>::Rank;
   static constexpr size_t Nc = DeviceGaugeFieldTypeTraits<GaugeFieldType>::Nc;
   static constexpr GaugeFieldType kind =
-      DeviceGaugeFieldTypeTraits<GaugeFieldType>::kind;
+      DeviceGaugeFieldTypeTraits<GaugeFieldType>::Kind;
+  GaugeFieldType gauge_field;
+  AdjFieldType adjoint_field;
 
   const IndexArray<rank> dimensions;
   real_t eps;
+  constGaugeField<rank, Nc> staple_field;
 
   UpdateMomentumGauge() = delete;
   ~UpdateMomentumGauge() = default;
 
   // TODO: Add Force application
+
+  template <typename... Indices>
+  KOKKOS_FORCEINLINE_FUNCTION void operator()(const Indices... Idcs) const {
+    // Update the momentum field
+    adjoint_field(Idcs...) +=
+        eps * gauge_field(Idcs...) *
+        staple_field(Idcs...); // TODO: add beta as an input somewhere
+  }
+
+  void update(const real_t step_size) override {
+    eps = step_size;
+    IndexArray<rank> start;
+    for (size_t i = 0; i < rank; ++i) {
+      start[i] = 0;
+    }
+    staple_field = stapleField(gauge_field.field);
+    // launch the kernel
+    tune_and_launch_for<rank>("UpdateMomentumGauge", start, dimensions, *this);
+    Kokkos::fence();
+  }
 };
 
 } // namespace klft
