9af502a1dcebbb58181e7d18a15c182a976bc58f
Dominik <rolfes1dominik@gmail.com>
2025-07-08 17:47:14 +0200

    errors

--- include/GaugeFieldHelper.hpp
+++ include/GaugeFieldHelper.hpp
@@ -84,7 +84,7 @@ void unitarity_restore(const typename DGaugeFieldType::type &field) {
                                                             field.dimensions);
   if constexpr (rank == 4) {
     tune_and_launch_for<rank>(
-        "UnitaryRestore", rp,
+        "UnitaryRestore", IndexArray<rank>{0}, field.dimensions,
         KOKKOS_LAMBDA(const index_t i0, const index_t i1, const index_t i2,
                       const index_t i3) {
           for (index_t mu = 0; mu < rank; ++mu) {
@@ -93,7 +93,7 @@ void unitarity_restore(const typename DGaugeFieldType::type &field) {
         });
   } else if constexpr (rank == 3) {
     tune_and_launch_for<rank>(
-        "UnitaryRestore", rp,
+        "UnitaryRestore", IndexArray<rank>{0}, field.dimensions,
         KOKKOS_LAMBDA(const index_t i0, const index_t i1, const index_t i2) {
           for (index_t mu = 0; mu < rank; ++mu) {
             field(i0, i1, i2, mu) = restoreSUN(field(i0, i1, i2, mu));
@@ -101,7 +101,7 @@ void unitarity_restore(const typename DGaugeFieldType::type &field) {
         });
   } else if constexpr (rank == 2) {
     tune_and_launch_for<rank>(
-        "UnitaryRestore", rp,
+        "UnitaryRestore", IndexArray<rank>{0}, field.dimensions,
         KOKKOS_LAMBDA(const index_t i0, const index_t i1) {
           for (index_t mu = 0; mu < rank; ++mu) {
             field(i0, i1, mu) = restoreSUN(field(i0, i1, mu));
