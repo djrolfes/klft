e06cb0f86316df32a0ac6a3e22d88b43e4a26fc4
Dominik <rolfes1dominik@gmail.com>
2025-07-16 12:42:40 +0200

    del the local  archive

--- arc/binaries_20250708/CMakeFiles/hmc.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for hmc.
-# This may be replaced when dependencies are built.
--- arc/binaries_20250708/tmp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-./hmc --filename b25.yaml
-./hmc --filename b30.yaml
-./hmc --filename b35.yaml
-./hmc --filename b40.yaml
-./hmc --filename b45.yaml
-./hmc --filename b50.yaml
