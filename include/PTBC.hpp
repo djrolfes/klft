e57b9d8fc9d35edb44be1d260bcdd198f1c4044b
Dominik <rolfes1dominik@gmail.com>
2025-08-05 16:08:19 +0200

    MPI should be correct, lets get debugging

--- include/PTBC.hpp
+++ include/PTBC.hpp
@@ -32,16 +32,16 @@ class PTBC { // do I need the AdjFieldType here?
                 GaugeFieldKind::PTBC);
   static_assert(isDeviceGaugeFieldType<DGaugeFieldType>::value);
   static_assert(isDeviceAdjFieldType<DAdjFieldType>::value);
-  constexpr static size_t rank =
+  constexpr static size_t Nd =
       DeviceGaugeFieldTypeTraits<DGaugeFieldType>::Rank;
   constexpr static size_t Nc = DeviceGaugeFieldTypeTraits<DGaugeFieldType>::Nc;
-  static_assert((rank == DeviceAdjFieldTypeTraits<DAdjFieldType>::Rank) &&
+  static_assert((Nd == DeviceAdjFieldTypeTraits<DAdjFieldType>::Rank) &&
                 (Nc == DeviceAdjFieldTypeTraits<DAdjFieldType>::Nc));
 
   using GaugeField = typename DGaugeFieldType::type;
   using AdjointField = typename DAdjFieldType::type;
   using HField = HamiltonianField<DGaugeFieldType, DAdjFieldType>;
-  using Update_Q = UpdatePositionGauge<rank, Nc, GaugeFieldKind::PTBC>;
+  using Update_Q = UpdatePositionGauge<Nd, Nc, GaugeFieldKind::PTBC>;
   using Update_P = UpdateMomentumGauge<DGaugeFieldType, DAdjFieldType>;
   using HMCType = HMC<DGaugeFieldType, DAdjFieldType, RNG>;
 
@@ -56,9 +56,14 @@ public:
 
   const index_t initial_index;
   index_t current_index;
+  index_t previous_index;
 
-  bool swap_first{
-      false}; // flag to indicate if this is the PTBC to initialize the swap
+  typedef enum {
+    TAG_DELTAS = 0,
+    TAG_ACCEPT = 1,
+    TAG_INDEX = 2,
+    TAG_SWAPSTART = 3
+  } MPI_Tags;
 
   PTBC() = delete; // default constructor is not allowed
 
@@ -97,8 +102,8 @@ public:
 
   void measure(GaugeObservableParams &gaugeObsParams, index_t step) {
     // measure the gauge observables
-    measureGaugeObservables<rank, Nc>(hamiltonian_field.gauge_field,
-                                      gaugeObsParams, step);
+    measureGaugeObservables<Nd, Nc>(hamiltonian_field.gauge_field,
+                                    gaugeObsParams, step);
   }
 
   void measure(SimulationLoggingParams &simLogParams, index_t step,
@@ -107,26 +112,35 @@ public:
     addLogData(simLogParams, step, hmc.delta_H, acc_rate, accept, time);
   }
 
-  void determineSwapStart() {
-    // rank 0 determines the swap start and Broadcasts it to the other ranks
-    this->swap_first = false;
-    index_t rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    int swap_start{0};
-    if (rank == 0) {
-      swap_start = int(dist(mt) * (size + 1));
-    }
-    MPI_Bcast(&swap_start, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    // printf("Rank %d of %d, swap_start = %d\n", rank, size, swap_start);
+  int step() { return hmc.hmc_step(); }
 
-    if (rank == swap_start) {
-      this->swap_first =
-          true; // this PTBC instance is the first in the swap order
-    }
+  real_t swap_rank(index_t rank) {
+    // return the rank of the partner device for the given rank
   }
 
-  int step() { return hmc.hmc_step(); }
+  real_t swap_partner(index_t partner_rank) {
+    // swaps the Defect with the partner rank and returns the partial Delta_S
+    previous_index = current_index;
+    MPI_Sendrecv(&current_index, 1, MPI_INT, partner_rank, TAG_INDEX,
+                 &current_index, 1, MPI_INT, partner_rank, TAG_INDEX,
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    real_t S_ss = -params.hmc_params.beta / 3 * getPlaquetteAroundDefect();
+    // now do the index/defect swapping
+    hamiltonian_field.gauge_field.template set_defect<index_t>(
+        params.defects[current_index]); // set the defect value of the current
+    //
+    real_t S_sr = -params.hmc_params.beta / 3 * getPlaquetteAroundDefect();
+    return S_sr - S_ss; // return the partial Delta_S
+  }
+
+  void reverse_swap(bool accept) {
+    if (!accept) {
+      current_index = previous_index;
+      hamiltonian_field.gauge_field.template set_defect<index_t>(
+          params.defects[current_index]); // set the defect value of the current
+    }
+  }
 
   int swap() {
     // check if this PTBC instance is the first within the swap order, otherwise
@@ -140,64 +154,66 @@ public:
     real_t partner_defect_value;
     bool accept = false;
     real_t Delta_S{0};
+    int swap_start{0};
+    index_t swap_rank{0};
 
-    if (this->swap_first) {
-      // send first and then receive
-      partner_rank = (rank + 1) % size;
-      MPI_Sendrecv(&current_index, 1, MPI_INT, partner_rank, 0, &partner_index,
-                   1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
-                   MPI_STATUS_IGNORE);
-
-      real_t S_ss = -params.hmc_params.beta / 3 * getPlaquetteAroundDefect();
-      // now do the index/defect swapping
-      std::swap(current_index, partner_index);
-      hamiltonian_field.gauge_field.template set_defect<index_t>(
-          params.defects[current_index]); // set the defect value of the current
-      //
-      real_t S_sr = -params.hmc_params.beta / 3 * getPlaquetteAroundDefect();
-      // index
-      real_t Delta_S_partner{0};
-      MPI_Recv(&Delta_S_partner, 1, MPI_REAL, partner_rank, 0, MPI_COMM_WORLD,
-               MPI_STATUS_IGNORE);
-
-      Delta_S = Delta_S_partner + S_sr - S_ss;
-      accept = true; // accept the swap if Delta_S < 0
-      if (Delta_S > 0) {
-        if (dist(mt) > Kokkos::exp(-Delta_S)) {
-          accept = false; // reject the swap
-        }
-      }
-      // TODO:...
-
-      partner_rank = (rank - 1) % size;
-      MPI_Sendrecv(&current_index, 1, MPI_INT, partner_rank, 0, &partner_index,
-                   1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
-                   MPI_STATUS_IGNORE);
-
+    if (rank == 0) {
+      swap_start = int(dist(mt) * (size + 1));
+      MPI_Bcast(&swap_start, 1, MPI_INT, TAG_SWAPSTART, MPI_COMM_WORLD);
     } else {
-      // receive first and then send
-      partner_rank = (rank - 1) % size;
-      MPI_Sendrecv(&current_index, 1, MPI_INT, partner_rank, 0, &partner_index,
-                   1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
-                   MPI_STATUS_IGNORE);
-      real_t S_rr = -params.hmc_params.beta / 3 * getPlaquetteAroundDefect();
-      // now do the index/defect swapping
-      std::swap(current_index, partner_index);
-      hamiltonian_field.gauge_field.template set_defect<index_t>(
-          params.defects[current_index]); // set the defect value of the current
-      //
-      real_t S_rs = -params.hmc_params.beta / 3 * getPlaquetteAroundDefect();
-      Delta_S = S_rs - S_rr;
-      accept = false;
-      MPI_Sendrecv(&Delta_S, 1, MPI_DOUBLE, partner_rank, 0, &accept, 1,
-                   MPI_C_BOOL, partner_rank, 0, MPI_COMM_WORLD,
-                   MPI_STATUS_IGNORE);
-      // index
+      MPI_Bcast(&swap_start, 1, MPI_INT, TAG_SWAPSTART, MPI_COMM_WORLD);
+    }
+    for (index_t i = 0; i < size; ++i) {
+      swap_rank = (swap_start + i) % size;
+      partner_rank = (swap_rank + 1) % size;
+      if (rank == swap_rank) {
+        Delta_S = swap_partner(partner_rank);
+        MPI_Send(&Delta_S, 1, MPI_REAL, 0, TAG_DELTAS, MPI_COMM_WORLD);
+        MPI_Send(&current_index, 1, MPI_INT, 0, TAG_INDEX, MPI_COMM_WORLD);
+      }
+      if (rank == partner_rank) {
+        Delta_S = swap_partner(swap_rank);
+        MPI_Send(&Delta_S, 1, MPI_REAL, 0, TAG_DELTAS, MPI_COMM_WORLD);
+        MPI_Send(&current_index, 1, MPI_INT, 0, TAG_INDEX, MPI_COMM_WORLD);
+      }
 
-      partner_rank = (rank + 1) % size;
-      MPI_Sendrecv(&current_index, 1, MPI_INT, partner_rank, 0, &partner_index,
-                   1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
-                   MPI_STATUS_IGNORE);
+      if (rank == 0) {
+        // gather the Delta_S values from all ranks
+        real_t Delta_S_partner1, Delta_S_partner2;
+        MPI_Recv(&Delta_S_partner1, 1, MPI_REAL, swap_rank, TAG_DELTAS,
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&Delta_S_partner2, 1, MPI_REAL, partner_rank, TAG_DELTAS,
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        Delta_S = Delta_S_partner1 + Delta_S_partner2;
+        accept = true; // accept the swap if Delta_S < 0
+        if (Delta_S > 0) {
+          if (dist(mt) > Kokkos::exp(-Delta_S)) {
+            accept = false; // reject the swap
+          }
+        }
+        MPI_Send(&accept, 1, MPI_C_BOOL, swap_rank, TAG_ACCEPT, MPI_COMM_WORLD);
+        MPI_Send(&accept, 1, MPI_C_BOOL, partner_rank, TAG_ACCEPT,
+                 MPI_COMM_WORLD);
+      }
+
+      if (rank == swap_rank || rank == partner_rank) {
+        // receive the accept value from rank 0
+        MPI_Recv(&accept, 1, MPI_C_BOOL, 0, 0, MPI_COMM_WORLD,
+                 MPI_STATUS_IGNORE);
+        reverse_swap(accept); // reverse the swap if not accepted
+      }
+
+      if (rank == 0 && accept) {
+        index_t swap_index, swap_partner_index;
+        MPI_Recv(&swap_index, 1, MPI_INT, swap_rank, TAG_INDEX, MPI_COMM_WORLD,
+                 MPI_STATUS_IGNORE);
+        MPI_Recv(&swap_partner_index, 1, MPI_INT, partner_rank, TAG_INDEX,
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        std::swap(params.defects[swap_index],
+                  params.defects[swap_partner_index]);
+      }
+      MPI_Bcast(params.defects.data(), params.defects.size(), MPI_REAL, 0,
+                MPI_COMM_WORLD);
     }
   }
 
@@ -205,7 +221,9 @@ public:
     // calculate the plaquette around the defect
     // this is a placeholder function, implement the actual plaquette
     // calculation
-    return 0.0; // return a dummy value for now
+    return GaugePlaquette<Nd, Nc, GaugeFieldKind::PTBC>(
+        hamiltonian_field
+            .gauge_field); // TODO: implement only calculating around the defect
   }
 
 private:
@@ -338,7 +356,6 @@ int run_PTBC(PTBCParams ptbc_params, RNG &rng,
   for (size_t step = 0; step < hmc_params.nsteps; ++step) {
     timer.reset();
 
-    ptbc.determineSwapStart(); // determine if this PTBC instance is the
     bool accept = ptbc.step();
 
     // maybe HMC Gauge Obeservables here
