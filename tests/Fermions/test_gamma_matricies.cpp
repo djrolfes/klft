82ae67d2eb37ce627e2f31d66e2efe61271af578
BenediktAuer <52496736+BenediktAuer@users.noreply.github.com>
2025-08-28 12:40:11 +0200

    [Update] Gammamatrix test

--- tests/Fermions/test_gamma_matricies.cpp
+++ tests/Fermions/test_gamma_matricies.cpp
@@ -2,17 +2,19 @@
 #include <iostream>
 
 #include "../../include/GammaMatrix.hpp"
+#include "Global.hpp"
+#include "Spinor.hpp"
 
 using namespace klft;
 
 #define HLINE "=========================================================\n"
 template <size_t RepDim>
-void print_matrix(const GammaMat<RepDim> &mat) {
+void print_matrix(const GammaMat<RepDim>& mat) {
   std::cout << std::fixed;
   for (int i = 0; i < 4; ++i) {
     std::cout << "[ ";
     for (int j = 0; j < 4; ++j) {
-      const auto &val = mat(i, j);
+      const auto& val = mat(i, j);
       std::cout << "(" << std::setw(6) << std::setprecision(2) << val.real()
                 << (val.imag() >= 0 ? "+" : "") << std::setw(6)
                 << std::setprecision(2) << val.imag() << "i) ";
@@ -22,8 +24,9 @@ void print_matrix(const GammaMat<RepDim> &mat) {
   std::cout << "\n";
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const* argv[]) {
   std::cout << HLINE << "Testing Gamma matrices" << HLINE;
+  GammaMat<4> ggamma_id = get_identity<4>();
   GammaMat<4> ggamma0 = get_gamma0();
   GammaMat<4> ggamma1 = get_gamma1();
   GammaMat<4> ggamma2 = get_gamma2();
@@ -58,6 +61,25 @@ int main(int argc, char const *argv[]) {
   std::cout << (vec_gamma[2] == ggamma2) << "\n";
   std::cout << "Gamma3:\n";
   std::cout << (vec_gamma[3] == ggamma3) << "\n";
+  Spinor<2, 4> spinor;
+  spinor[0][0] = complex_t(1.0, 0.0);
+  spinor[0][1] = complex_t(3, 0);
+  spinor[0][2] = complex_t(7, 0);
+  spinor[0][3] = complex_t(9, 0);
+  spinor[1][0] = complex_t(5, 0);
+  spinor[1][1] = complex_t(6, 0);
+  spinor[1][2] = complex_t(7, 0);
+  spinor[1][3] = complex_t(8, 0);
+  spinor[2][0] = complex_t(9, 0);
+  spinor[2][1] = complex_t(10, 0);
+  spinor[2][2] = complex_t(11, 0);
+  spinor[2][3] = complex_t(12, 0);
+  print_spinor_int(spinor);
+  auto res1 = (ggamma_id - ggamma3) * spinor;
+  auto res2 = project(3, -1, spinor);
+  print_spinor_int(res1, " (I + gamma1) * spinor");
+  print_spinor_int(res2, "P_-0*spinor");
+  printf("Are equal: %i", res1 == res2);
   /* code */
   return 0;
 }
