e879fab753c7c015caa3afb404663615a55a8af8
Dominik <rolfes1dominik@gmail.com>
2025-09-29 15:25:56 +0200

    saving is important

--- include/ActionDensity.hpp
+++ include/ActionDensity.hpp
@@ -78,6 +78,7 @@ real_t getActionDensity(const typename DGaugeFieldType::type g_in) {
   Kokkos::fence();
 
   real_t density = -2.0 * Kokkos::real(plaq_per_site.sum());
+  density += plaq_per_site.size();
   Kokkos::fence();
 
   return density;
