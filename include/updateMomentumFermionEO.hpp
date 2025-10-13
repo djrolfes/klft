d9b4f0ce442cb255684bdb1a5e8f52a06e069872
BenediktAuer <52496736+BenediktAuer@users.noreply.github.com>
2025-10-13 15:58:03 +0200

    [Update] Use sepearte Kernels for even and odd contribution of the FermionForce

--- include/updateMomentumFermionEO.hpp
+++ include/updateMomentumFermionEO.hpp
@@ -26,9 +26,11 @@
 
 namespace klft {
 
-template <typename DSpinorFieldType, typename DGaugeFieldType,
+template <typename DSpinorFieldType,
+          typename DGaugeFieldType,
           typename DAdjFieldType,
-          template <template <typename, typename> class DiracOpT, typename,
+          template <template <typename, typename> class DiracOpT,
+                    typename,
                     typename> class _Solver,
           template <typename, typename> class DiracOpT>
 class UpdateMomentumWilsonEO : public UpdateMomentum {
@@ -75,9 +77,11 @@ class UpdateMomentumWilsonEO : public UpdateMomentum {
   UpdateMomentumWilsonEO() = delete;
   ~UpdateMomentumWilsonEO() = default;
 
-  UpdateMomentumWilsonEO(FermionField& phi_, const GaugeFieldType& gauge_field_,
+  UpdateMomentumWilsonEO(FermionField& phi_,
+                         const GaugeFieldType& gauge_field_,
                          AdjFieldType& adjoint_field_,
-                         const diracParams& params_, const real_t& tol_)
+                         const diracParams& params_,
+                         const real_t& tol_)
       : UpdateMomentum(0),
         phi(phi_),
         gauge_field(gauge_field_),
@@ -89,7 +93,8 @@ class UpdateMomentumWilsonEO : public UpdateMomentum {
     sigma = FermionField(phi.dimensions, 0);
     y = FermionField(phi.dimensions, 0);
   }
-
+  struct TagEvenContribution {};
+  struct TagOddContribution {};
   // Implemntation of the force correspondig to the Hermitian Wilson dirac
   // Operator
   //* S_F  = \phi^{\dagger} * M^{-1} * \phi
@@ -108,80 +113,80 @@ class UpdateMomentumWilsonEO : public UpdateMomentum {
   // +\kappa \gamma_5 \delta_{x+\mu,y} (1+\gamma_\mu) t_i
   // U_\mu(z)^\dagger\delta_{z,y} <- Have to Check this
   template <typename... Indices>
-  KOKKOS_FORCEINLINE_FUNCTION void operator()(const Indices... Idcs) const {
-    Kokkos::Array<size_t, rank> idx{Idcs...};
-    //     // compute parity of the site:
-    //     int parity = 0;
-    // #pragma unroll
-    //     for (index_t i = 0; i < rank; ++i) {
-    //       parity += idx[i];
-    //     }
-    //     parity &= 1;
-    auto x_half = index_full_to_half(idx);
-    switch (x_half.second) {
-      case 0:  // Even
-        for (size_t mu = 0; mu < rank; ++mu) {
-          // X = chi , Y = chi_alt
+  KOKKOS_FORCEINLINE_FUNCTION void operator()(TagEvenContribution,
+                                              const Indices... Idcs) const {
+    Kokkos::Array<size_t, rank> x_half{Idcs...};
 
-          auto xp = shift_index_plus_bc<rank, size_t>(
-              Kokkos::Array<size_t, rank>{Idcs...}, mu, 1, 3, -1,
-              this->gauge_field.dimensions);  // odd
-          auto xp_half = index_full_to_half(xp.first);
-          KOKKOS_ASSERT(xp_half.second == 1);
-          auto X_proj = project(mu, -1, this->sigma(xp_half.first));
-          // minus sign in the projector comes from the derivative of D
-          auto Xplus =
-              (-1 * this->params.kappa * this->params.kappa * xp.second) *
-              X_proj;
-          auto temp1 = reconstruct(mu, -1, gauge_field(Idcs..., mu) * Xplus);
-          auto deriv = temp1 * gamma5(conj(this->chi(x_half.first)));
+    auto idx = index_half_to_full(x_half, 0);
 
-          auto Y_proj =
-              project_alt(mu, 1, gamma5(conj(this->rho(xp_half.first))));
-          auto YPlus =
-              (this->params.kappa * this->params.kappa * xp.second) * Y_proj;
-          auto temp3 = (reconstruct_alt(
-              mu, 1, YPlus * conj(this->gauge_field(Idcs..., mu))));
-          deriv += this->y(x_half.first) * temp3;
+    // Even
+    for (size_t mu = 0; mu < rank; ++mu) {
+      // X = chi , Y = chi_alt
 
-          momentum(Idcs..., mu) -= 2 * eps *
-                                   traceT(traceLessAntiHermitian(
+      auto xp = shift_index_plus_bc<rank, index_t>(
+          Kokkos::Array<index_t, rank>{idx}, mu, 1, 3, -1,
+          this->gauge_field.dimensions);  // odd
+      auto xp_half = index_full_to_half(xp.first);
+      KOKKOS_ASSERT(xp_half.second == 1);
+      auto X_proj = project(mu, -1, this->sigma(xp_half.first));
+      // minus sign in the projector comes from the derivative of D
+      auto Xplus =
+          (-1 * this->params.kappa * this->params.kappa * xp.second) * X_proj;
+      auto temp1 = reconstruct(mu, -1, gauge_field(idx, mu) * Xplus);
+      auto deriv = temp1 * gamma5(conj(this->chi(x_half)));
 
-                                       deriv));
-        }
-        break;
-      case 1:  // Odd:
-        for (size_t mu = 0; mu < rank; ++mu) {
-          // X = chi , Y = chi_alt
-          // Ned to go back to half index
-          auto xp = shift_index_plus_bc<rank, size_t>(
-              Kokkos::Array<size_t, rank>{Idcs...}, mu, 1, 3, -1,
-              this->gauge_field.dimensions);  // even
-          auto xp_half = index_full_to_half(xp.first);
-          KOKKOS_ASSERT(xp_half.second == 0);
-          auto X_proj = project(mu, -1, this->y(xp_half.first));
-          // minus sign in the projector comes from the derivative of D
-          auto Xplus =
-              (-1 * this->params.kappa * this->params.kappa * xp.second) *
-              X_proj;
-          auto temp1 = reconstruct(mu, -1, gauge_field(Idcs..., mu) * Xplus);
-          auto deriv = temp1 * gamma5(conj(this->rho(x_half.first)));
+      auto Y_proj = project_alt(mu, 1, gamma5(conj(this->rho(xp_half.first))));
+      auto YPlus =
+          (this->params.kappa * this->params.kappa * xp.second) * Y_proj;
+      auto temp3 =
+          (reconstruct_alt(mu, 1, YPlus * conj(this->gauge_field(idx, mu))));
+      deriv += this->y(x_half) * temp3;
 
-          auto Y_proj =
-              project_alt(mu, 1, gamma5(conj(this->chi(xp_half.first))));
-          auto YPlus =
-              (this->params.kappa * this->params.kappa * xp.second) * Y_proj;
-          auto temp3 = (reconstruct_alt(
-              mu, 1, YPlus * conj(this->gauge_field(Idcs..., mu))));
-          deriv += this->sigma(x_half.first) * temp3;
+      momentum(idx, mu) -= 2 * eps *
+                           traceT(traceLessAntiHermitian(
 
-          momentum(Idcs..., mu) -= 2 * eps *
-                                   traceT(traceLessAntiHermitian(
-
-                                       deriv));
-        }
-        break;
+                               deriv));
     }
+
+    // X = chi , Y = chi_alt
+    // Checkboard 0:
+  }
+  // iterations space is here the odd index space
+  template <typename... Indices>
+  KOKKOS_FORCEINLINE_FUNCTION void operator()(TagOddContribution,
+                                              const Indices... Idcs) const {
+    Kokkos::Array<size_t, rank> x_half{Idcs...};
+
+    auto idx = index_half_to_full(x_half, 1);
+
+    for (size_t mu = 0; mu < rank; ++mu) {
+      // X = chi , Y = chi_alt
+      // Ned to go back to half index
+      auto xp = shift_index_plus_bc<rank, index_t>(
+          Kokkos::Array<index_t, rank>{idx}, mu, 1, 3, -1,
+          this->gauge_field.dimensions);  // even
+      auto xp_half = index_full_to_half(xp.first);
+      KOKKOS_ASSERT(xp_half.second == 0);
+      auto X_proj = project(mu, -1, this->y(xp_half.first));
+      // minus sign in the projector comes from the derivative of D
+      auto Xplus =
+          (-1 * this->params.kappa * this->params.kappa * xp.second) * X_proj;
+      auto temp1 = reconstruct(mu, -1, gauge_field(idx, mu) * Xplus);
+      auto deriv = temp1 * gamma5(conj(this->rho(x_half)));
+
+      auto Y_proj = project_alt(mu, 1, gamma5(conj(this->chi(xp_half.first))));
+      auto YPlus =
+          (this->params.kappa * this->params.kappa * xp.second) * Y_proj;
+      auto temp3 =
+          (reconstruct_alt(mu, 1, YPlus * conj(this->gauge_field(idx, mu))));
+      deriv += this->sigma(x_half) * temp3;
+
+      momentum(idx, mu) -= 2 * eps *
+                           traceT(traceLessAntiHermitian(
+
+                               deriv));
+    }
+
     // X = chi , Y = chi_alt
     // Checkboard 0:
   }
@@ -215,8 +220,10 @@ class UpdateMomentumWilsonEO : public UpdateMomentum {
     }
     // print_SUNAdj(momentum(1, 0, 0, 0, 0), "Before Update Momentum");
     // launch the kernel
-    tune_and_launch_for<rank>("UpdateMomentumWilson", start,
-                              gauge_field.dimensions, *this);
+    tune_and_launch_for<rank, TagEvenContribution>(
+        "UpdateMomentumWilson", start, this->phi.dimensions, *this);
+    tune_and_launch_for<rank, TagOddContribution>("UpdateMomentumWilson", start,
+                                                  this->phi.dimensions, *this);
     // print_SUNAdj(momentum(1, 0, 0, 0, 0), "After Update Momentum");
 
     Kokkos::fence();
