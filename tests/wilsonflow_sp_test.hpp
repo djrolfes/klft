0d96c427c24d1d87d52568ae415cafb33d9ef6a4
Dominik <rolfes1dominik@gmail.com>
2025-10-20 12:01:24 +0200

    improved wilsonflow does not seem to work

--- tests/wilsonflow_sp_test.hpp
+++ tests/wilsonflow_sp_test.hpp
@@ -8,75 +8,6 @@
 #include "SimulationLogging.hpp"
 
 namespace klft {
-// template <typename DGaugeFieldType>
-// void get_sp_distribution(const typename DGaugeFieldType::type gauge_field,
-//                          std::vector<real_t> &rtn, real_t max = 0.1,
-//                          real_t bin_width = 0.001) {
-//   static const size_t Nd = DeviceGaugeFieldTypeTraits<DGaugeFieldType>::Rank;
-//   static_assert(Nd == 4);
-//   static const size_t Nc = DeviceGaugeFieldTypeTraits<DGaugeFieldType>::Nc;
-//   static const GaugeFieldKind k =
-//       DeviceGaugeFieldTypeTraits<DGaugeFieldType>::Kind;
-//   using GaugeFieldType = typename DGaugeFieldType::type;
-//   using FieldType = typename DeviceFieldType<Nd>::type;
-//   FieldType plaq_per_site(gauge_field.dimensions, complex_t(0.0, 0.0));
-//
-//   // number of bins
-//   size_t nbins = static_cast<size_t>(max / bin_width);
-//   rtn.resize(nbins, 0.0);
-//
-//   // 1. Create a Kokkos View on the device for the histogram.
-//   // Initialize it to zeros.
-//   Kokkos::View<real_t *> rtn_d("rtn_d", nbins);
-//   Kokkos::deep_copy(rtn_d, 0.0);
-//
-//   GaugePlaq<Nd, Nc, k> GPlaq(gauge_field, plaq_per_site,
-//                              gauge_field.dimensions);
-//
-//   // tune_and_launch_for<Nd>("compute plaq_per_site", IndexArray<Nd>{0},
-//   //                         gauge_field.dimensions, GPlaq);
-//   // Kokkos::fence();
-//
-//   tune_and_launch_for<Nd>(
-//       "binning sp's", IndexArray<Nd>{0}, gauge_field.dimensions,
-//       KOKKOS_LAMBDA(size_t i0, size_t i1, size_t i2, size_t i3) {
-//         for (index_t mu = 0; mu < Nd; ++mu) {
-//           for (index_t nu = 0; nu < Nd; ++nu) {
-//             if (nu > mu) {
-//               real_t s = Kokkos::real(2.0 - GPlaq(mu, nu, i0, i1, i2, i3));
-//               // now find the end bin:
-//               index_t endbin =
-//                   static_cast<index_t>(nbins * Kokkos::min(1.0, s / max));
-//               endbin = endbin =
-//                   0 ? 1 : endbin; // ensure at least one bin is filled
-//               for (index_t i = 0; i < endbin; i++) {
-//                 Kokkos::atomic_inc(&rtn_d[i]);
-//               }
-//             }
-//           }
-//         }
-//       });
-//   Kokkos::fence();
-//
-//   Kokkos::View<real_t *>::HostMirror rtn_h =
-//   Kokkos::create_mirror_view(rtn_d);
-//
-//   // Perform the device-to-host copy.
-//   Kokkos::deep_copy(rtn_h, rtn_d);
-//
-//   // 4. Copy data from the host mirror into the final std::vector.
-//   for (size_t i = 0; i < nbins; ++i) {
-//     rtn[i] = rtn_h(i);
-//   }
-//
-//   auto volume = Nd * Nd;
-//   for (size_t vol : gauge_field.dimensions) {
-//     volume *= vol;
-//   }
-//   for (index_t i = 0; i < size(rtn); i++) {
-//     rtn[i] /= volume;
-//   }
-// }
 
 template <typename DGaugeFieldType>
 real_t get_spmax(const typename DGaugeFieldType::type gauge_field) {
@@ -136,10 +67,6 @@ real_t get_spavg(const typename DGaugeFieldType::type gauge_field) {
   GaugePlaq<Nd, Nc, k> GPlaq(gauge_field, plaq_per_site,
                              gauge_field.dimensions);
 
-  // tune_and_launch_for<Nd>("compute plaq_per_site", IndexArray<Nd>{0},
-  //                         gauge_field.dimensions, GPlaq);
-  // Kokkos::fence();
-
   real_t rtn = 0.0;
   auto policy = Policy<Nd>({0, 0, 0, 0}, gauge_field.dimensions);
   Kokkos::parallel_reduce(
@@ -160,8 +87,6 @@ real_t get_spavg(const typename DGaugeFieldType::type gauge_field) {
       },
       rtn);
   Kokkos::fence();
-
-  // Kokkos::printf("gaugefield size: %lu\n", gauge_field.field.size());
 
   return rtn / gauge_field.field.size();
 }
@@ -233,7 +158,7 @@ index_t do_wflowtest(HMCType &hmc, GaugeObservableParams &gaugeObsParams,
   }
 
   // Set precision for floating point numbers in the output file
-  output_file_topologicalcharge << std::fixed << std::setprecision(8);
+  output_file_topologicalcharge << std::fixed << std::setprecision(12);
   output_file_actiondensity << std::fixed << std::setprecision(12);
   output_file_actiondensity_clover << std::fixed << std::setprecision(12);
   output_file_actiondensity_0 << std::fixed << std::setprecision(12);
@@ -280,7 +205,7 @@ index_t do_wflowtest(HMCType &hmc, GaugeObservableParams &gaugeObsParams,
 
   for (int flow_Step = 1; flow_Step <= flow_steps; ++flow_Step) {
     // perform wilson flow step
-    wilson_flow.flow_DBW2();
+    wilson_flow.flow();
 
     flow_times.push_back(flow_Step * gaugeObsParams.wilson_flow_params.eps);
     topological_charges.push_back(
@@ -404,7 +329,7 @@ index_t do_wflowtest(HMCType &hmc, GaugeObservableParams &gaugeObsParams,
 
       for (int flow_Step = 1; flow_Step <= flow_steps; ++flow_Step) {
         // perform wilson flow step
-        wilson_flow.flow_DBW2();
+        wilson_flow.flow();
 
         flow_times.push_back(flow_Step * gaugeObsParams.wilson_flow_params.eps);
         topological_charges.push_back(
